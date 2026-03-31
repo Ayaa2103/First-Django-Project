c53647a4816a3fb1d2eaf3af61bff9a6114a2300
Ayaa2103 <aya.belhou@gmail.com>
2026-03-31 17:15:50 +0100

    Create django app

--- storefront/settings.py
+++ storefront/settings.py
@@ -35,10 +35,10 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    'django.contrib.contenttypes', 
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'playground'
 ]
 
 MIDDLEWARE = [
